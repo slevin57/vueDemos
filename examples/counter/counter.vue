5cbf74e63c07b275caff24caf70c407af6612c94
pc <575720013@qq.com>
2018-03-29 13:13:23 +0800

    vuex-counter例子完成

--- examples/counter/counter.vue
+++ examples/counter/counter.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="app">
-      d
-      {{count}}
+        Number is {{$store.state.count}},</br>
+        <button @click="increment()">increment +</button> <button @click="decrement()">decrement -</button> </br>
+        <button @click="incrementIfEven()">increment +</button> if Number is even</br>
+        <button @click="decrementIfOdd()">decrement -</button> if Numver is odd</br>
+        <button @click="incrementAsync()">increment +</button> async.</br>
+        <button @click="decrementAsyncByDispatch()">derecment -</button> async.</br>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapMutations,mapActions } from 'vuex'
 
 export default {
     data () {
         return {
-            
+
         }
     },
     computed:{
-        ...mapState(['count'])
+        
     },
-    mounted(){
-
+    methods:{
+        ...mapMutations([
+            'increment',
+            'decrement',
+        ]),
+        ...mapActions([
+            'decrementIfOdd',
+            'incrementIfEven',
+            'incrementAsync',
+        ]),
+        decrementAsyncByDispatch(){
+            this.$store.dispatch('decrementAsync')
+        }
     }
 }
 </script>
--- examples/counter/store.js
+++ examples/counter/store.js
@@ -10,15 +10,48 @@ const state = {
 }
 
 const getters = {
-
+    evenOrOdd: state => state.count % 2 === 0 ? 'even' : 'odd'
 }
 
 const mutations = {
-
+    increment: state => state.count++,
+    decrement: state => state.count--,
+    /**不能下面这样写，mutation中获取不到getter返回的状态 */
+    // incrementIfEven: (state) => {
+    //     console.log(getters.evenOrOdd);
+    //     if (getters.evenOrOdd === 'even') {
+    //         state.count++;
+    //     }
+    // },
 }
 
 const actions = {
-
+    incrementIfEven: (context) => {
+        if (context.getters.evenOrOdd === 'even') {
+            context.commit('increment')
+        }
+    },
+    decrementIfOdd: ({getters,commit}) => {
+        if (getters.evenOrOdd == 'odd') {
+            commit('decrement');
+        }
+    },
+    incrementAsync({commit}){
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                commit('increment');
+                resolve();
+            }, 1000);
+        })
+    },
+    decrementAsync ({commit}) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                commit('decrement');
+                resolve();
+            }, 1000);
+        })
+    }
 }
 
 export default new Vuex.Store({
