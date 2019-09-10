762bd93600544c7d69a4fa85666b00d0aaff6c0c
slevin <575720013@qq.com>
2019-09-10 11:30:21 +0800

    优化infinite-scroll指令

--- examples/loadimg/components/loadimg.vue
+++ examples/loadimg/components/loadimg.vue
@@ -1,8 +1,8 @@
 <template>
-<div>
-    <section class="wrapper" v-load-data="loadDataOpt">
-        <ul class="content" >
-            <li v-for="(item,index) in lists">
+<div class="app-comp" >
+    <section class="wrapper" ref="wrapper" v-infinite-scroll="loadDataOpt1">
+        <ul class="content" v-infinite-scroll="loadDataOpt2">
+            <li v-for="(item,index) in lists" >
                 <div class="pic" v-load-img="item.imgLink">
                     <!-- <img src="../assets/pic1.jpeg" alt="本地图片" srcset=""> -->
                     <img :src="item.imgLink" alt="Bing每日图片超高清" />
@@ -11,17 +11,19 @@
             </li>
         </ul>
     </section>    
-    <p>加载提示：{{loadHint}}</p>
+    <p class="wrapper-hint"> 滚动wrapper触发。wrapper加载提示：执行加载数据的方法{{wrapperLoadNum}}</p>
+    <p class="window-hint">滚动窗口触发。window加载提示：执行加载数据的方法{{windowLoadNum}}</p>
 </div>
 </template>
 
 <script>
+import dt from '../../utils/dt'
     export default {
         name: 'directives-loadimg',
         data () {
             return {
-                loadHint:'',//加载提示
-                loadNum:'',//加载次数
+                wrapperLoadNum: 0,//加载数
+                windowLoadNum: 0,
                 API: 'https://api.douban.com/v2/movie/coming_soon',//豆瓣开发者平台
                 lists: [
                     // {title:'去年元月时',imgLink:require('../../assets/pic1.jpeg')},//本地图片
@@ -35,8 +37,14 @@
                     {title:'不见去年人',imgLink:'http://api.dujin.org/bing/1920.php'},
                     {title:'泪湿春衣袖',imgLink:'http://api.dujin.org/bing/1920.php'},
                 ],
-                loadDataOpt:{ //加载数据的配置
-                    loadDataFn: this.loadDataFn, //加载的方法
+                // 处理window滚动事件的配置
+                loadDataOpt1:{ 
+                    loadFn: this.watchWindow, 
+                },
+                // 处理wrapper滚动事件的配置
+                loadDataOpt2:{ 
+                    loadFn: this.watchWrapper, 
+                    ref: 'wrapper'
                 }
             }
         },
@@ -61,68 +69,55 @@
                     }
                 }
             },
-            /**@滚动加载数据
-            * 用在出现滚动条的元素上；
-            * 使用时传入一个配置对象，其中包含一个loadDataFn的加载数据的方法
-            * v-load-data="loadDataOpt.loadDataFn"
-            */
-            loadData: {
-                bind (el,binding) {
-                    el.addEventListener('scroll',binding.def.watchScroll.bind(null,binding));
+            /**
+             * 滚动加载
+             * 用于列表项的包裹元素上，接收一个对象作为参数
+             * @loadFn {function} 必选。为数据加载的方法，当**指令绑定的元素底边出现在视窗内**便会触发此方法
+             * @ref {string} 可选。指定监听滚动事件的目标对象，需将其`ref`值传入。默认为空，则会监听window的滚动事件
+             */
+            infiniteScroll: {
+                inserted(el, binding, vnode) {
+                    let ref = binding.value.ref;
+                    let wrapperEl = ref ? vnode.context.$refs[ref] : "";
+                    let listenEl = wrapperEl ? wrapperEl : window;
+                    let params = {
+                        wrapperEl, el, binding
+                    };
+                    // 对监听事件统一做防抖处理
+                    listenEl.addEventListener("scroll", dt.debounce(binding.def.scrollHandler.bind(null, params), 100));
                 },
-                unbind (el,binding) {
-                    el.removeEventListener('scroll',binding.def.watchScroll);
+                unbind(el, binding, vnode) {
+                    let ref = binding.value.ref;
+                    let wrapperEl = ref ? vnode.context.$refs[ref] : "";
+                    let listenEl = wrapperEl ? wrapperEl : window;
+                    listenEl.removeEventListener("scroll", binding.def.scrollHandler);
                 },
                 /**
-                因为removeEventListener需要两个参数，第二个参数要指定移除的函数名称，
-                所以监听滚动的方法不能定义在bind钩子中，
-                因此选择在指令对象loadData上定义监听滚动的方法，
-                此方法在钩子函数中可以通过binding.def 访问到，然后通过bind()方法把用到的钩子函数的参数传入 。
-                */
-                watchScroll (binding,evt) {
-                    // 通过事件对象evt去获取自身高度、滚动高度及子元素的高度；也可以通过钩子函数中的el参数获取
-                    let scrollTop = evt.target.scrollTop,
-                        wrapperHeight = evt.target.clientHeight,
-                        contentHeight = evt.target.childNodes[0].clientHeight;
-                    
-                    if(scrollTop + wrapperHeight >= contentHeight) {
-                        // 执行传入的加载数据的方法
-                        binding.value.loadDataFn();
+                 * 因为removeEventListener需要两个参数，第二个参数要指定移除的函数名称，
+                 * 所以监听滚动的方法不能定义在bind钩子中，
+                 * 因此选择在指令对象loadData上定义监听滚动的方法，
+                 * 此方法在钩子函数中可以通过binding.def 访问到，然后通过bind()方法把用到的钩子函数的参数传入 。
+                 */
+                scrollHandler(params, evt) {
+                    // 可以通过事件对象evt去获取自身高度、滚动高度及子元素的高度；也可以通过钩子函数中的el参数获取
+                    // console.log(`evt.target:`,evt.target.scrollTop);
+                    let { wrapperEl, el, binding } = params;
+                    let rect = el.getBoundingClientRect();
+                    let targetBottom = wrapperEl ? wrapperEl.getBoundingClientRect().bottom : innerHeight;
+                    if (rect.bottom <= targetBottom) {
+                        binding.value.loadFn();
                     }
-                }
-            },
-            /** 错误示例
-            // 监听滚动执行的方法不能写在钩子函数内
-            // 否则解除监听的时候获取不到
-            loadData: {
-                bind: function (el,binding,vnode) {
-                        el.addEventListener('scroll',(evt)=>{
-                            // 通过el去获取自身高度、滚动高度及子元素的高度
-                            let scrollTop = el.scrollTop,
-                                wrapperHeight = el.clientHeight,
-                                contentHeight = el.childNodes[0].clientHeight;
-                            
-                            if(scrollTop + wrapperHeight >= contentHeight) {
-                                // 执行传入的加载数据的方法
-                                binding.value.loadDataFn();
-                            }
-
-                            // 通过事件对象获取自身数据及子元素数据也是可以的
-                            // console.log('evt.target.height:',evt.target.clientHeight);
-                            // console.log('evt.target.scrollTop:',evt.target.scrollTop);
-                            // console.log('99:',evt.target.childNodes[0].clientHeight);
-                            // // console.log('99:',evt.target.children[0].clientHeight);
-                        })
                 },
-                unbind: function (el) {
-                    el.removeEventListener('scroll');
-                }
-            } */
+            },
         },
         methods: {
-            loadDataFn() {
-                this.loadHint = `执行加载数据的方法${this.loadNum++}`
-                // console.log('加载数据的方法:',this.$http.get);
+            watchWindow() {
+                console.log(`33:`,33);
+                this.windowLoadNum++;
+            },
+            watchWrapper() {
+                console.log(`2:`,2);
+                this.wrapperLoadNum++;
             }
         }
     }
@@ -130,6 +125,9 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.app-comp {
+    height: 2000px;
+}
 ul,li{
     list-style: none;
     margin: 0;
@@ -142,6 +140,16 @@ ul,li{
     overflow-y: scroll;
     border:1px solid #333333;
 }
+.wrapper-hint{
+    text-align: center;
+}
+.window-hint {
+    width: 100%;
+    position: fixed;
+    bottom: 50px;
+    text-align: center;
+}
+
 .content li{
     width: 220px;
     margin: 0 auto;
